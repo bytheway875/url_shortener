0c407058f3019a6cf713d64fc176c607d4996dfd
Shannon Byrne <s.byrne2254@gmail.com>
2013-08-16 02:04:06 -0500

    Changed shortened_url in migration

--- db/migrate/20130816024200_create_urls.rb
+++ db/migrate/20130816024200_create_urls.rb
@@ -2,7 +2,7 @@ class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
       t.string :original_url
-      t.string :shortened_extension
+      t.string :shortened_url
       t.integer :status, default: 301 
 
       t.timestamps
